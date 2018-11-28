348d3df3e0876dc2a12122a8f85bc83ad6c2c6ff
Murphy Zheng <murphy@mieasy.com>
2018-11-28 16:46:03 +0800

    Add files

---
+++ MAClusterAnimator.h
@@ -0,0 +1,17 @@
+//
+//  MAClusterAnimator.h
+//  officialDemo2D
+//
+//  Created by Jason-autonavi on 15/3/13.
+//  Copyright (c) 2015年 AutoNavi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MAAnimator.h"
+#import <UIKit/UIKit.h>
+
+
+@interface MAClusterAnimator : NSObject< MAAnimator>
+
+
+@end
